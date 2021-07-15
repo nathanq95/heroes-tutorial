e4329721d79c7f48d863338b3be4c8049e28e110
nathanq95 <nathanquiocson@gmail.com>
2021-07-15 00:02:54 -0700

    created message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,12 +3,14 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heros';
 import { Observable, of } from 'rxjs';
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
